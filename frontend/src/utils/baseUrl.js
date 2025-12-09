00b408acb5f10a7e53d7ef6d29b3b6d168ed6a77
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-12-09 14:24:36 +0530

    asdad

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 export const getBaseUrl=()=>{
     if(process.env.NODE_ENV==='production'){
-        return 'https://www.yourproductiondomain.com';
+        return 'https://book-store-bk5a.vercel.app';
     }
     else{
         return 'http://localhost:3001';
